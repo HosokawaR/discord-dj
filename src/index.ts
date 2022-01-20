2b6087f7772a64a6c75ecc8d6a2b793ec1a1a5ff
HosokawaR <thin.river.jp@gmail.com>
2022-01-20 21:41:20 +0900

    delete server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client, Intents } from "discord.js"
+import { Client, Intents, Interaction } from "discord.js"
 import { HOST_NAME, PORT, token } from "./config"
 import { deployCommand, commands } from "./deploy-commands"
 import { canCommandBot, commandTypeGard } from "./utls"
@@ -41,41 +41,5 @@ client.on("interactionCreate", async (interaction) => {
         }
     }
 })
+
 ;(async () => await deployCommand)()
-
-const server = http.createServer((req, res) => {
-    const url = req?.url || ""
-    if (!url.startsWith("/add")) {
-        res.statusCode = 404
-        res.end(`There is no matched content for ${url}`)
-        return
-    }
-
-    if (req.method === "OPTIONS") {
-        res.statusCode = 200
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        res.setHeader(
-            "Access-Control-Allow-Headers",
-            "X-Requested-With, Content-Type"
-        )
-        res.end()
-    } else if (req.method === "POST") {
-        let body = ""
-        req.on("data", (chunk) => {
-            body += chunk
-        })
-
-        req.on("end", async () => {
-            const content = JSON.parse(body)
-            console.log("リクエスト", content)
-            await addTrack(client, content.query, content.name)
-            res.statusCode = 200
-            res.end()
-        })
-    }
-})
-
-server.listen(PORT, HOST_NAME, () => {
-    console.log(`Server running at http://${HOST_NAME}:${PORT}/`)
-})
